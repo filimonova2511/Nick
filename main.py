0b2e9c7aa5233621436c9ddc5c9d89f1130dfdd3
Yengeniya Filimonova <filimonova25-11@mail.ru>
2022-06-07 18:25:48 +0300

    add class Limbs and driving in class Human

--- main.py
+++ main.py
@@ -20,9 +20,16 @@ class Body:
         sweat_ml = random.randint(10, 1000)
         print(f'выделилось {sweat_ml} мл пота')
 
+class Limbs:   # добавить функции
+    arms = 2
+    feet = 2
 
-class Human(Head, Body):
-    pass
+class Human(Head, Body, Limbs):   # можно добавить танцевать, гулять (сколько км прошел),ходить на свидание или в бар
+    def drive(self):
+        driving = "Прекрасное вождение- уровень 'Шумахер'" if random.randint(0, 1) else 'Опять лишили прав'
+        print(driving)
+
+
 
 
 nick = Human()
